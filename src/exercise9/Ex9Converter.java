dc0046fec76ca3c2ac553e2a3976f549268623bf
woolsa <woolsa01@luther.edu>
2017-11-22 16:56:57 -0600

    exercise9

--- src/exercise9/Ex9Converter.java
+++ src/exercise9/Ex9Converter.java
@@ -2,30 +2,32 @@ package exercise9;
 
 /**
  * Class Ex9Converter
- * @author yasiro01
+ * @author woolsa01
  */
 public class Ex9Converter {
+    public Ex9Converter() {}
+    
   public Double c2f(Double valueFrom) {
-    throw new UnsupportedOperationException();
-  }
+    return ((valueFrom * 9 / 5) + 32);
+      }
 
   public Double c2k(Double valueFrom) {
-    throw new UnsupportedOperationException();
+    return (valueFrom + 273.15);
   }
 
   public Double f2c(Double valueFrom) {
-    throw new UnsupportedOperationException();
+    return ((valueFrom - 32) * 5 / 9);
   }
 
   public Double f2k(Double valueFrom) {
-    throw new UnsupportedOperationException();
+    return (((valueFrom -32) *5 / 9) + 273.15);
   }
 
   public Double k2c(Double valueFrom) {
-    throw new UnsupportedOperationException();
+    return (valueFrom - 273.15);
   }
 
   public Double k2f(Double valueFrom) {
-    throw new UnsupportedOperationException();
+    return (((valueFrom - 273.15) * 9 / 5) + 32);
   }
 }
